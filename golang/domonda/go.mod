5a604b8977c5d599e906bb08f35dcc06585bd511
Erik Unger <erik@erikunger.com>
2021-04-27 15:06:10 +0200

    added golang/domonda.UploadDocument

---
+++ golang/domonda/go.mod
@@ -0,0 +1,8 @@
+module github.com/domonda/api/golang/domonda
+
+go 1.16
+
+require (
+	github.com/domonda/go-types v0.0.0-20210423112527-2c20ef27e5bd
+	github.com/ungerik/go-fs v0.0.0-20210316184417-d8a3b2b8ca48
+)
---
+++ golang/domonda/upload.go
@@ -0,0 +1,75 @@
+package domonda
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+
+	"github.com/domonda/go-types/uu"
+	"github.com/ungerik/go-fs"
+)
+
+func UploadDocument(ctx context.Context, apiKey string, documentCategory uu.ID, documentFile, invoiceFile fs.FileReader, tags ...string) (documentID uu.ID, err error) {
+	body := bytes.NewBuffer(nil)
+	form := multipart.NewWriter(body)
+
+	err = form.WriteField("documentCategory", documentCategory.String())
+	if err != nil {
+		return uu.IDNil, err
+	}
+
+	for _, tag := range tags {
+		err = form.WriteField("tag", tag)
+		if err != nil {
+			return uu.IDNil, err
+		}
+	}
+
+	documentWriter, err := form.CreateFormFile("document", documentFile.Name())
+	if err != nil {
+		return uu.IDNil, err
+	}
+	_, err = documentFile.WriteTo(documentWriter)
+	if err != nil {
+		return uu.IDNil, err
+	}
+
+	if invoiceFile != nil {
+		invoiceWriter, err := form.CreateFormFile("invoice", invoiceFile.Name())
+		if err != nil {
+			return uu.IDNil, err
+		}
+		_, err = invoiceFile.WriteTo(invoiceWriter)
+		if err != nil {
+			return uu.IDNil, err
+		}
+	}
+
+	request, err := http.NewRequest("POST", "https://domonda.app/api/public/document/upload", body)
+	if err != nil {
+		return uu.IDNil, err
+	}
+
+	request.Header.Add("Authorization", "Bearer "+apiKey)
+	request.Header.Add("Content-Type", "multipart/form-data")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return uu.IDNil, err
+	}
+
+	if response.StatusCode != 200 {
+		return uu.IDNil, fmt.Errorf("%d: %s", response.StatusCode, response.Status)
+	}
+
+	defer response.Body.Close()
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		return uu.IDNil, err
+	}
+
+	return uu.IDFromBytes(responseBody)
+}
