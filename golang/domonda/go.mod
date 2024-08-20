f557406299d86f45d9c93c043f88e746a2b7151c
Erik Unger <erik@erikunger.com>
2024-08-20 15:43:40 +0200

    added golang/domonda.Partner

--- golang/domonda/go.mod
+++ golang/domonda/go.mod
@@ -3,13 +3,28 @@ module github.com/domonda/api/golang/domonda
 go 1.23
 
 require (
-	github.com/domonda/go-types v0.0.0-20240304130021-550340237017
-	github.com/ungerik/go-fs v0.0.0-20240302080707-ebffc0d04c7c
+	github.com/domonda/go-types v0.0.0-20240820131329-129bfec7bd19
+	github.com/ungerik/go-fs v0.0.0-20240702143946-3ecb6733945d
 )
 
 require (
-	github.com/domonda/go-errs v0.0.0-20240301142737-8fde935c9bd4 // indirect
+	github.com/cention-sany/utf7 v0.0.0-20170124080048-26cad61bd60a // indirect
+	github.com/domonda/go-errs v0.0.0-20240702051036-0e696c849b5f // indirect
 	github.com/domonda/go-pretty v0.0.0-20240110134850-17385799142f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/gogs/chardet v0.0.0-20211120154057-b7413eaefb8f // indirect
+	github.com/jaytaylor/html2text v0.0.0-20230321000545-74c2419ad056 // indirect
+	github.com/jhillyerd/enmime v1.2.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
+	github.com/teamwork/tnef v0.0.0-20200108124832-7deabccfdb32 // indirect
+	github.com/ungerik/go-reflection v0.0.0-20240110134735-61cada706fec // indirect
+	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	mvdan.cc/xurls/v2 v2.5.0 // indirect
 )
---
+++ golang/domonda/partner.go
@@ -0,0 +1,60 @@
+package domonda
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/domonda/go-types/account"
+	"github.com/domonda/go-types/bank"
+	"github.com/domonda/go-types/country"
+	"github.com/domonda/go-types/email"
+	"github.com/domonda/go-types/notnull"
+	"github.com/domonda/go-types/nullable"
+	"github.com/domonda/go-types/vat"
+)
+
+type Partner struct {
+	Name             string
+	AlternativeNames notnull.StringArray // used when merging
+
+	// main location
+	Street    nullable.TrimmedString
+	City      nullable.TrimmedString
+	ZIP       nullable.TrimmedString
+	Country   country.NullableCode
+	Phone     nullable.TrimmedString
+	Email     email.NullableAddress
+	Website   nullable.TrimmedString
+	CompRegNo nullable.TrimmedString
+	TaxIDNo   nullable.TrimmedString
+	VATIDNo   vat.NullableID
+
+	// partner accounts
+	VendorAccountNumber account.NullableNumber // "" means not set -> will not create a partner account
+	ClientAccountNumber account.NullableNumber // "" means not set -> will not create a partner account
+
+	IBAN  bank.NullableIBAN
+	BIC   bank.NullableBIC
+	IBAN2 bank.NullableIBAN
+	BIC2  bank.NullableBIC
+}
+
+func (p *Partner) String() string {
+	var b strings.Builder
+	b.WriteString(p.Name)
+	if p.VATIDNo.IsNotNull() {
+		b.WriteString(" ")
+		b.WriteString(string(p.VATIDNo))
+	}
+	return b.String()
+}
+
+func (p *Partner) VendorAccountNumberUint() uint64 {
+	u, _ := strconv.ParseUint(p.VendorAccountNumber.String(), 10, 64)
+	return u
+}
+
+func (p *Partner) ClientAccountNumberUint() uint64 {
+	u, _ := strconv.ParseUint(p.ClientAccountNumber.String(), 10, 64)
+	return u
+}
