74685424dfc092ba3980308ec4474113f8e7923f
Erik Unger <erik@erikunger.com>
2024-08-21 13:16:15 +0200

    complete domonda.BankAccount

--- golang/domonda/bankaccount.go
+++ golang/domonda/bankaccount.go
@@ -2,17 +2,34 @@ package domonda
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/domonda/go-types/bank"
+	"github.com/domonda/go-types/money"
 	"github.com/domonda/go-types/notnull"
+	"github.com/domonda/go-types/nullable"
 )
 
+// BankAccount represents a checking account
 type BankAccount struct {
-	Holder notnull.TrimmedString
-	IBAN   bank.IBAN
-	BIC    bank.BIC
+	IBAN     bank.IBAN
+	BIC      bank.BIC
+	Currency money.Currency
+	Holder   notnull.TrimmedString
+
+	// Optional
+	AccountNumber nullable.TrimmedString
+	Name          nullable.TrimmedString
+	Description   nullable.TrimmedString
 }
 
 func PostBankAccounts(ctx context.Context, apiKey string, bankAccounts []*BankAccount) error {
-	panic("TODO: implement me")
+	response, err := postJSON(ctx, apiKey, "/masterdata/bank-accounts", bankAccounts)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != 200 {
+		return fmt.Errorf("unexpected status code: %d", response.StatusCode)
+	}
+	return nil
 }
